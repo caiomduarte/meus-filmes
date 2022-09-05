1b7e4610051d02511b88157eb26ec81d3fb2fe07
= <=>
2022-09-05 20:42:52 -0300

    Criando a tela Home

---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import {
+        View, 
+        Text, 
+        TextInput, 
+        TouchableOpacity, 
+        StyleSheet
+    }  from 'react-native';
+
+export default function Home(){
+    return(
+        <View>
+            <Text>Meus Filmes</Text>
+            <Text>Olá Caio, boa noite!</Text>
+
+            
+        </View>
+    );
+}
+
+//Criando os estilos
+const styles = StyleSheet.create({
+ 
+});
